c736b9a1f6fcc789089711916a35e2078037c82b
Eric Lewis <eric.andrew.lewis@gmail.com>
2016-11-02 10:32:33 -0400

    Finish up step example

--- src/editor-5.js
+++ src/editor-5.js
@@ -4,7 +4,7 @@ const {DOMParser, Schema} = require("prosemirror-model")
 const {schema: baseSchema} = require("prosemirror-schema-basic")
 const {addListNodes} = require("prosemirror-schema-list")
 const {exampleSetup} = require("prosemirror-example-setup")
-const {ReplaceStep} = require("prosemirror-transform")
+const {ReplaceStep, AddMarkStep, RemoveMarkStep, ReplaceAroundStep} = require("prosemirror-transform")
 const schema = new Schema({
   nodes: addListNodes(baseSchema.nodeSpec, "paragraph block*", "block"),
   marks: baseSchema.markSpec
@@ -34,6 +34,16 @@ const renderStep = (step) => {
       text += `Replace the content between ${step.from} and ${step.to} with the slice: \n`
       text += JSON.stringify(step.slice.toJSON(), null, 2)
     }
+  }
+  if (step instanceof AddMarkStep) {
+    text += `For the content between ${step.from} and ${step.to} apply a ${step.mark.type.name} mark`
+  }
+  if (step instanceof RemoveMarkStep) {
+    text += `For the content between ${step.from} and ${step.to} remove the ${step.mark.type.name} mark`
+  }
+  if (step instanceof ReplaceAroundStep) {
+    text += `For the content between ${step.from} and ${step.to}, remove the outer node and insert its content into\n`
+    text += JSON.stringify(step.slice.toJSON(), null, 2)
   }
   let pre = document.createElement('pre')
   pre.style.backgroundColor = colors[Math.floor( Math.random() * 4)]
